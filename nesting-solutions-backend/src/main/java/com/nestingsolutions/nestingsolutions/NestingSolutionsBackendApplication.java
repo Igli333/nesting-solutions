b4bc27130d007be46141ba2601d52a20b9e974fb
Igli Balla <igliballa312@gmail.com>
2023-10-20 23:25:22 +0200

    Initial Backend Commit

---
+++ nesting-solutions-backend/src/main/java/com/nestingsolutions/nestingsolutions/NestingSolutionsBackendApplication.java
@@ -0,0 +1,14 @@
+package com.nestingsolutions.nestingsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestingSolutionsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestingSolutionsBackendApplication.class, args);
+	}
+
+
+}
---
+++ nesting-solutions-backend/src/test/java/com/nestingsolutions/nestingsolutions/NestingSolutionsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestingsolutions.nestingsolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestingSolutionsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
