1264f5ce063cbbbe399e5b5e76c5c124e1569ae6
Igli Balla <igliballa312@gmail.com>
2023-10-21 08:06:11 +0200

    Mapper bean declaration

--- nesting-solutions-backend/src/main/java/com/nestingsolutions/nestingsolutions/NestingSolutionsBackendApplication.java
+++ nesting-solutions-backend/src/main/java/com/nestingsolutions/nestingsolutions/NestingSolutionsBackendApplication.java
@@ -1,7 +1,9 @@
 package com.nestingsolutions.nestingsolutions;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NestingSolutionsBackendApplication {
@@ -11,4 +13,8 @@ public class NestingSolutionsBackendApplication {
 	}
 
 
+	@Bean
+	ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
