2b54e698fd4fa1f0a4097c0f70d68bc6cc8a2772
AlgerPire <58697445+AlgerPire@users.noreply.github.com>
2023-10-22 12:57:38 +0200

    dorm + dashboard

--- nesting-solutions-frontend/src/app/pages/dashboard/dashboard.component.ts
+++ nesting-solutions-frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -14,6 +14,13 @@ export class DashboardComponent implements OnInit {
   public clicked: boolean = true;
   public clicked1: boolean = false;
   public clicked2: boolean = false;
+  studentsList: any[] = [
+    {id:1, name:"Alger Pire", university:"UPT", contractEndDate:"31-12-2023", roomType: "Single Room"},
+    {id:2, name:"Armand Gjoka", university:"UT", contractEndDate:"31-05-2024", roomType: "Double Room"},
+    {id:3, name:"Elton Nore", university:"EPOKA", contractEndDate:"28-02-2024", roomType: "Single Room"},
+    {id:4, name:"Altina Sesi", university:"UM", contractEndDate:"01-03-2024", roomType: "1 Bedroom"},
+    {id:5, name:"Xhoana Nole", university:"UB", contractEndDate:"31-07-2024", roomType: "2 Bedroom"},
+  ]
 
   constructor() {}
 
